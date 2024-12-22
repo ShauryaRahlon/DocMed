439c7029cf805c4ef8182b7cfa05aeda99e3b1b5
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2024-12-23 04:08:10 +0530

    Fixing Routes

--- Client/src/Footer/Footer.jsx
+++ Client/src/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Footer.css';
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-   <>
-    <footer class="footer">
-        <p>Copyright © DocMed - All Rights Reserved | <a href="#">Privacy Policy</a></p>
-    </footer>
-   </>
+    <>
+      <footer class="footer">
+        <p>Copyright © DocMed - All Rights Reserved | <Link to='/privacy'>Privacy Policy</Link></p>
+      </footer>
+    </>
   )
 }
 
