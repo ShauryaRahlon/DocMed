58cac4ea85704136f85b5635d9c10380b33450a2
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2024-12-24 03:00:28 +0530

    Bugs

--- Client/src/Components/MainHome.jsx
+++ Client/src/Components/MainHome.jsx
@@ -9,6 +9,7 @@ import Header from './Header.jsx';
 import Carousel from './Carousel.jsx';
 import OnlineCard from './OnlineCard.jsx';
 
+
 function MainHome() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -74,6 +75,7 @@ function MainHome() {
         </div>
       </main>
       <Card />
+
       <Carousel />
 
     </>
