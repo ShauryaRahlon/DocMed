76900951fa1323d7120181cae237b059c4179993
Vishwas-0612 <srivastavavishwas01@gmail.com>
2024-12-24 05:32:19 +0530

    Connect page

--- Client/src/Pages/Connect.jsx
+++ Client/src/Pages/Connect.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { useSocket } from '../Providers/Socket'
+import { useSocket } from '../Providers/Socket';
+import '../Styling/Connect.css';
+import { Link } from 'react-router-dom';
+
 const Connect = () => {
     const { socket } = useSocket();
     const navigate = useNavigate();
@@ -25,13 +28,33 @@ const Connect = () => {
     }
 
     return (
-        <div>
+        <div className='connect-container'>
+            <div className='icon-container'>
+              <Link to='/'><img src="/public/Logo2.webp" alt="Logo icon" /></Link>
+            </div>
             <div>
-                <input value={emailId} onChange={e => setemailId(e.target.value)} type='email' placeholder='emailId'></input>
-                <input value={roomId} onChange={e => setRoomId(e.target.value)} type='text' placeholder='Enter Room code'></input>
-                <button onClick={handleJoinRoom}>Join</button>
+            <div className='actual-container'>
+                {/*<div className='email-container'>
+                    <label htmlFor="email">Email: </label>
+                    <input className='connect-email' value={emailId} onChange={e => setemailId(e.target.value)} type='email' placeholder='emailId'></input>
+                </div>
+                <div className='room-container'>
+                    <input className='room-code' value={roomId} onChange={e => setRoomId(e.target.value)} type='text' placeholder='Enter Room code'></input>
+                </div>
+                <div btn-container>
+                    <button className='join-btn' onClick={handleJoinRoom}>Join</button>
+                </div>
+            </div> */}
+
+                <form className="form">
+                    <div className="title">Welcome!</div>
+                    <input type="email" placeholder="Email" name="email" className="input" value={emailId} onChange={e => setemailId(e.target.value)} />
+                    <input type="text" placeholder="Room Code" name="password" className="input" value={roomId} onChange={e => setRoomId(e.target.value)} />
+                    <button className="button-confirm" onClick={handleJoinRoom}>Join →</button>
+                </form>
             </div>
         </div>
+    </div>
     )
 }
 
